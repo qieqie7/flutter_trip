a7f7bcbdffcff3e4cf22c5e39ae1fc7069318537
ykst615 <excit1ng@126.com>
2019-05-16 22:32:13 +0800

    init

---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
