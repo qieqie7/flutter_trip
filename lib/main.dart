d2e23ecdcbc83a574ac278d91f1e97f939be3dbc
ykst615 <excit1ng@126.com>
2019-05-20 22:58:10 +0800

    finish tab navigator bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trip/navigator/tab_navigator.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TabNavigator()
     );
   }
 }
