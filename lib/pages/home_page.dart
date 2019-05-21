d09374bd9a4ee67c2d5c5a4145f67d458cc173cc
ykst615 <excit1ng@126.com>
2019-05-22 01:12:48 +0800

    home page add banner image from internet

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,22 +7,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List _imageList = [
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1558468657919&di=be97e197dadca55b0ff6937d1cde77dd&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01cc2e5947ddeca8012193a3eabef4.jpg%402o.jpg',
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1558468700071&di=b3dc0c7ecd60a948209356de0514ca1b&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01ce8b582439aea84a0e282ba855d9.jpg',
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1558468700071&di=b6cbd6335ecd82e56f070e8155d4214d&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0100b057e915da0000018c1b3911be.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           children: <Widget>[
-            // Container(
-            //   height: 160,
-            //   child: Swiper(
-            //     itemCount: 2,
-            //     itemBuilder: (BuildContext context, int index) {
-            //       return Image.network('src');
-            //     },
-            //     pagination: SwiperPagination(),
-            //   ),
-            // )
+            Container(
+              height: 160,
+              child: Swiper(
+                itemCount: _imageList.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _imageList[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
           ],
         ),
       ),
